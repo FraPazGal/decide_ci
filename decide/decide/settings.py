2c20404e5d4a2cc7f9adf01954d06433d2947b2b
FraPazGal <frandepaz@yahoo.es>
2019-12-02 12:08:46 +0100

    Commit

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://decide-practica-fpg-1920.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
